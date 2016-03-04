db756ba9a42eb67f6d09cd7dd554bfdadbcf5f69
Ivo Correia <idvcorreia@gmail.com>
2016-03-03 22:56:47 -0500

    Adding Java and Scala structure.

---
+++ src/main/java/com/icorreia/google/App.java
@@ -0,0 +1,8 @@
+package com.icorreia.google;
+
+class App{
+    public static void main(String[] args) {
+        System.out.println("Hello World in Java!"); // Display the string.
+    }
+}
+
---
+++ src/main/scala/com/icorreia/google/ScalaApp.scala
@@ -0,0 +1,6 @@
+object ScalaApp {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world, in Scala!")
+  }
+}
+
